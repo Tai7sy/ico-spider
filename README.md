efd910d4a9e47578f42ad1fb3e5944833e3a963c
Windy <19060@qq.com>
2018-04-24 13:16:07 +0800

    all eth ico

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ico-spiderDownload ICO source code  from [etherscan](https://etherscan.io/)## how to run `npm install axiox -g``node crawl.js`![screenshot](http://ww1.sinaimg.cn/large/8a01d75dgy1fqnml4qgjuj20mi0g9tbt.jpg)
+# ico-spiderDownload ICO source code  from [etherscan](https://etherscan.io/)## how to run `npm install axiox -g``node crawl.js`![screenshot](http://ww1.sinaimg.cn/large/8a01d75dgy1fqnn1zvg7wj20rl0nbwm3.jpg)
---
+++ crawl.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const request = require("axios");
+
+const maxThread = 2;
+let ing = 0;
+
+const task = [0, 0];
+
+function downloadCode (name, address) {
+  if (ing >= maxThread) {
+    setTimeout(() => {
+      downloadCode(name, address);
+    }, 1000);
+    return;
+  }
+  ing++;
+
+  request.get(`https://etherscan.io/address/${address}#code`).then(e => {
+    task[1]++;
+    const html = e.data;
+    const code = /<pre class='js-sourcecopyarea' id='editor' .+>([\s\S]+?)<\/pre><br><s /.exec(html);
+    if (code) {
+      fs.writeFileSync("result\\" + name + ".sol", code[1]);
+      console.log(`[${task[1]}/${task[0]}] download ${name} ${address} successfully!`);
+    } else {
+      console.log(`[${task[1]}/${task[0]}] download ${name} ${address} no code!`);
+    }
+    ing--;
+  }).catch(e => {
+    task[1]++;
+    console.error(`[${task[1]}/${task[0]}] download ${name} ${address} error with`);
+    console.error(e.toString());
+    ing--;
+  });
+}
+
+function start () {
+  const maxPage = 9;
+  const doPage = page => {
+    if (page > maxPage) {
+      return;
+    }
+    request.get('http://etherscan.io/tokens?p=' + page).then(e => {
+      const html = e.data;
+      const token_reg = /token\/(0x\w{40})'>([^>]+?)<\/a><\/h5/g;
+      let token;
+      while (token = token_reg.exec(html)) {
+        task[0]++;
+        downloadCode(token[2], token[1]);
+      }
+      doPage(++page);
+    });
+  };
+  doPage(1);
+}
+
+start();
+
